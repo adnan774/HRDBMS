1976ea6f2bc38c55901693c193a1be9b9d7d45fb
Adnan Patel <adnanpatel016@gmail.com>
2023-11-29 18:09:11 +0000

    Added getAllSalaries method to retrieve salary records in SalaryDAO.

--- HRDBMS1/src/main/java/database/SalaryDAO.java
+++ HRDBMS1/src/main/java/database/SalaryDAO.java
@@ -52,7 +52,55 @@ public class SalaryDAO {
 		}
 	}
 	
+	// Salaries Queries
 	
+		private Salaries getNextSalary(ResultSet rs){
+			Salaries thisSalary=null;
+			try {
+				thisSalary = new Salaries(
+						rs.getInt("salary_id"),
+						rs.getString("job_title"),
+						rs.getFloat("salary"));	
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    	return thisSalary;		
+		}
+		
+		public ArrayList<Salaries> getAllSalaries(){
+			   
+			ArrayList<Salaries> allSalaries= new ArrayList<Salaries>();
+			openConnection();
+			ResultSet rs1 = null;
+
+		   
+			try{
+			    String selectSQL = "SELECT salary_id, job_title, salary FROM salaries;";
+			    rs1 = stmt.executeQuery(selectSQL);
+		    
+			    while (rs1.next()) {
+			        Salaries salary = new Salaries();
+			        salary.setSalary_id(rs1.getInt("salary_id"));
+			        salary.setJob_title(rs1.getString("job_title"));
+			        salary.setSalary(rs1.getFloat("salary"));
+			        allSalaries.add(salary);
+			    }
+	        } catch (SQLException se) {
+	            System.out.println(se);
+	        } finally {
+	            if (rs1 != null) {
+	                try {
+	                    rs1.close();
+	                } catch (SQLException e) {
+	                    e.printStackTrace();
+	                }
+	            }
+	        }
+
+	        return allSalaries;
+	    }
+			
 		
 	}
 	
