98e6bb7115bb35f0dbb2ca9337beff831ea75aa2
Adnan Patel <adnanpatel016@gmail.com>
2023-11-29 19:35:08 +0000

    Implemented getAllEmployees method in EmployeeDAO for fetching employee data.

--- HRDBMS1/src/main/java/database/EmployeeDAO.java
+++ HRDBMS1/src/main/java/database/EmployeeDAO.java
@@ -92,6 +92,58 @@ public class EmployeeDAO {
 	    }
 	    return thisEmployee;
 	}
+	
+	public ArrayList<Employees> getAllEmployees() {
+	    ArrayList<Employees> allEmployees = new ArrayList<Employees>();
+	    openConnection();
+	    ResultSet rs1 = null;
+	    try {
+	    	String selectSQL = "SELECT e.emp_id, e.first_name, e.last_name, e.email, e.date_of_birth, e.phone_number, e.hire_date, e.address, e.city, e.town,"
+	    			+ " e.post_code, d.department_id, d.department_name, d.location, s.salary_id, s.job_title, s.salary FROM employees e "
+	    		    + "JOIN departments d ON e.department_id = d.department_id JOIN salaries s ON e.salary_id = s.salary_id ORDER BY e.emp_id;";
+	        rs1 = stmt.executeQuery(selectSQL);
+	        while (rs1.next()) {
+	            Departments department = new Departments(
+	                rs1.getInt("department_id"),
+	                rs1.getString("department_name"),
+	                rs1.getString("location")
+	            );
+	            Salaries salary = new Salaries(
+	                rs1.getInt("salary_id"),
+	                rs1.getString("job_title"),
+	                rs1.getFloat("salary")              
+	            );
+	            Employees employee = new Employees(
+	                rs1.getInt("emp_id"),
+	                rs1.getString("first_name"),
+	                rs1.getString("last_name"),
+	                rs1.getString("email"),
+	                rs1.getString("date_of_birth"),
+	                rs1.getString("phone_number"),
+	                rs1.getString("hire_date"),
+	                rs1.getString("address"),
+	                rs1.getString("city"),
+	                rs1.getString("town"),
+	                rs1.getString("post_code"),
+	                department,
+	                salary
+	            );
+	            System.out.println("Fetched employee: " + employee);
+	            allEmployees.add(employee);
+	        }
+	    } catch (SQLException se) {
+	        System.out.println(se);
+	    } finally {
+	        if (rs1 != null) {
+	            try {
+	                rs1.close();
+	            } catch (SQLException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    }
+	    return allEmployees;
+	}
 
 	}
 	
