9f0c2039b6956127df353f90e67e65d2aa50f66a
Adnan Patel <adnanpatel016@gmail.com>
2023-11-29 18:05:49 +0000

    Setup database connection in SalaryDAO for salary-related operations.

---
+++ HRDBMS1/src/main/java/database/SalaryDAO.java
@@ -0,0 +1,62 @@
+package database;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import models.Departments;
+import models.Employees;
+import models.Salaries;
+
+import java.sql.*;
+
+
+public class SalaryDAO {
+	
+	Employees oneEmployee = null;
+	Salaries oneSalary = null;
+	Departments oneDepartment = null;
+	Connection conn = null;
+    Statement stmt = null;
+    String user = "root1";
+    String password = "password1";
+    String url = "jdbc:mysql://localhost:3306/hrdbms";
+
+	
+    private void openConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (Exception e) {
+            System.out.println("Error loading the JDBC driver: " + e.getMessage());
+            e.printStackTrace();
+            return;
+        }
+
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            stmt = conn.createStatement();
+            System.out.println("Database connection successfully established.");
+        } catch (SQLException se) {
+            System.out.println("Error while connecting to the database: " + se.getMessage());
+            se.printStackTrace();
+        }
+    }
+
+	private void closeConnection(){
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+		
+	}
+	
+
+   
+   
+   
