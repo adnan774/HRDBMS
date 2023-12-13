4203516970b93348a4d68b6c9cab26757d8adbfb
Adnan Patel <adnanpatel016@gmail.com>
2023-12-13 14:17:08 +0000

    Added doPost method in EmployeeAPIController for employee creation

--- HRDBMS1/src/main/java/controllers/EmployeesAPIController.java
+++ HRDBMS1/src/main/java/controllers/EmployeesAPIController.java
@@ -46,5 +46,50 @@ public class EmployeesAPIController extends HttpServlet {
     }
     
     
+    // Handle POST requests to add a new employee
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        Gson gson = new Gson();
+        EmployeeDAO employeeDAO = new EmployeeDAO();
+        DepartmentDAO departmentDAO = new DepartmentDAO();
+        SalaryDAO salaryDAO = new SalaryDAO();
+
+        try {
+            // Parse the request body to an Employees object
+            Employees employee = gson.fromJson(request.getReader(), Employees.class);
+
+            // Extract department name and job title from the employee object
+            String departmentName = employee.getDepartments().getDepartment_name();
+            String jobTitle = employee.getSalaries().getJob_title();
+
+            // Look up the department ID and salary ID
+            int departmentId = departmentDAO.getDepartmentIdByName(departmentName);
+            int salaryId = salaryDAO.getSalaryIdByJobTitle(jobTitle);
+            
+            System.out.println("Resolved Department ID: " + departmentId);
+            System.out.println("Resolved Salary ID: " + salaryId);
+            
+            // Set the department ID and salary ID in the employee object
+            employee.getDepartments().setDepartment_id(departmentId);
+            employee.getSalaries().setSalary_id(salaryId);
+
+            // Save the employee
+            boolean isInserted = employeeDAO.insertEmployee(employee, departmentId, salaryId);
+            if (isInserted) {
+                response.getWriter().write(gson.toJson(employee));
+                response.setStatus(HttpServletResponse.SC_CREATED); // 201 Created status
+            } else {
+                response.getWriter().write("{\"message\": \"Employee not added. Please try again.\"}");
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST); // 400 Bad Request status
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            response.getWriter().write("{\"message\": \"Error: " + e.getMessage() + "\"}");
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR); // 500 Internal Server Error
+        }
+    }
+    
     
 }
